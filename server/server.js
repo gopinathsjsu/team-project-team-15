fbe8ab37893a1d0cce71d1331be447a3b1bfd4f3
svdpatibandla <saivamsidutt.patibandla@sjsu.edu>
2022-12-04 17:32:46 -0800

    updated api for assigning baggage

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import db from "./config/database.js";
 import Routes from "./routes/index.js";
 import cors from "cors";
 import cron from "node-cron";
-import gateRoutes from "./routes/index.js";
 import { gatecronfunction, maintaingatetemp } from "./Controllers/Schedulers/GateSchedulerFile.js";
 import { baggagecronfunction, maintainbagtemp } from "./Controllers/Schedulers/BaggageSchedulerFile.js";
 
@@ -27,5 +26,4 @@ global.db = db;
 app.use(cors());
 app.use(express.json());
 app.use('/', Routes);
-app.use('/gates', gateRoutes);
 app.listen(5001, () => console.log('Server running at port 5001'));
